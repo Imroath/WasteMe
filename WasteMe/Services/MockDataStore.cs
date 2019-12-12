2c376df3e7531565d80ddeda7f05047e044d50c9
jakros <49780040+jakros@users.noreply.github.com>
2019-12-12 11:10:58 +0100

    new prods (without proper pictures)

--- WasteMe/Services/MockDataStore.cs
+++ WasteMe/Services/MockDataStore.cs
@@ -14,25 +14,67 @@ namespace WasteMe.Services
         {
             items = new List<Item>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), 
-                    Name = "Permanent Market", 
-                    Barcode="4004675006400",
-                    Image="permanentmarker.jpg",
+                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Snickers",
+                    Barcode="5900951027307",
+                    Image="?.jpg",
                     WasteType="plastic",
                     WasteImage="plastic.png"
                 },
-                new Item { Id = Guid.NewGuid().ToString(),
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Milk",
+                    Barcode="5904215131458",
+                    Image="?.jpg",
+                    WasteType="paper",
+                    WasteImage="paper.png"
+                },
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Cane sugar",
+                    Barcode="5904215135395",
+                    Image="?.jpg",
+                    WasteType="paper",
+                    WasteImage="paper.png"
+                },
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Permanent marker",
+                    Barcode="4038375025287",
+                    Image="?.jpg",
+                    WasteType="plastic",
+                    WasteImage="plastic.png"
+                },
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Chewing gum",
+                    Barcode="4009900484695",
+                    Image="?.jpg",
+                    WasteType="plastic",
+                    WasteImage="plastic.png"
+                },
+                                new Item { Id = Guid.NewGuid().ToString(),
                     Name = "Coca-Cola",
                     Barcode="5449000000996",
                     Image="cola.jpg",
                     WasteType="plastic",
                     WasteImage="plastic.png"
                 },
-                new Item { Id = Guid.NewGuid().ToString(),
-                    Name = "Post-It",
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Tea",
+                    Barcode="3585804000090",
+                    Image="?.jpg",
+                    WasteType="metal",
+                    WasteImage="metal.png"
+                },
+                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "Post-it",
                     Barcode="4046719100675",
-                    Image="postit.jpg",
-                    WasteType="plastic",
+                    Image="?.jpg",
+                    WasteType="paper",
+                    WasteImage="paper.png"
+                },
+                                                new Item { Id = Guid.NewGuid().ToString(),
+                    Name = "GP Batteries",
+                    Barcode="4891199000034",
+                    Image="?.jpg",
+                    WasteType="paper",
                     WasteImage="paper.png"
                 }
             };
